69216fb7c9aedcc3f2fc6fde28ca14b039e3e196
blank71 <8blank71@gmail.com>
2024-10-17 11:00:42 +0900

    add CreateTodo

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,11 @@
-package graph
-
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+package graph
+
+import "github.com/blank71/gqlgen-learn/graph/model"
+
+type Resolver struct {
+	todos []*model.Todo
+}
